82c738d70a946def1327464a4a8a1ba7fb91c8ee
Sunuma Limbu <sunumayengden2@gmail.com>
2026-04-07 21:11:51 +0545

    03-expressions-variables-and-tdd

--- pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/02-variable-assignment/variable-assignment.js
@@ -1,1 +1,8 @@
 // YOUR CODE BELOW
+const myFirstName = 'Sunuma';
+const myFavoriteNum = 4;
+const havingFun = true;
+
+console.log(myFirstName)
+console.log(myFavoriteNum)
+console.log(havingFun)
--- pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
+++ pre-work-workshops/03-expressions-variables-and-tdd/05-take-the-shortcut/take-the-shortcut.js
@@ -1,3 +1,6 @@
 let happyNum = 1000;
 
 // YOUR CODE BELOW
+happyNum /=2;
+happyNum -=496;
+happyNum ++;
